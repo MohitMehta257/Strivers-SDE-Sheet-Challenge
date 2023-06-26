46d8fe148481c4cf1d59d009841731527e0dd149
MohitMehta257 <58682658+MohitMehta257@users.noreply.github.com>
2023-06-27 01:16:31 +0530

    Create Job Sequencing Problem.java

---
+++ Job Sequencing Problem.java
@@ -0,0 +1,29 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution
+{
+    public static int jobScheduling(int[][] jobs)
+    {
+        // Write your code here
+        int n=jobs.length;
+        Arrays.sort(jobs,(a,b)->b[1]-a[1]);
+        int ans=0;
+
+        boolean[] vis=new boolean[n];
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=Math.min(n-1,jobs[i][0]-1);j>=0;j--)
+            {
+                if(vis[j]==false)
+                {
+                    vis[j]=true;
+                    ans+=jobs[i][1];
+                    break;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
